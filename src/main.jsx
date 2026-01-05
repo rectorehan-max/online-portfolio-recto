e1a9425b8f54f6e0fa9115eef52de886953f4ad3
unknown <rectorehan@gmail.com>
2026-01-05 22:45:10 +0800

    Third Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
 )
