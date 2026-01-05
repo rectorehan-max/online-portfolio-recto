93c12bada4137d877dd75dc7309d7c10077eee4c
unknown <rectorehan@gmail.com>
2026-01-05 23:14:18 +0800

    fifth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { HashRouter as Router } from 'react-router-dom'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Router>
+//       <App />
+//     </Router>
+//   </StrictMode>
+// )
