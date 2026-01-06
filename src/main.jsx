60564782f91aebab340af96413ba308a48fac949
unknown <rectorehan@gmail.com>
2026-01-06 09:12:40 +0800

    main.jsx change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename={ import.meta.env.BASE_URL }>
       <App />
     </Router>
   </StrictMode>
